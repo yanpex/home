9e25c0ca8a848ba8c81298b721c1592123d73cf1
yanpex <87096808+yanpex@users.noreply.github.com>
2021-07-07 23:48:36 +0100

    all

--- mail.php
+++ mail.php
@@ -1,20 +1,2 @@
-<?php
-$email = $_POST['con_email'];
-$name = $_POST['con_name'];
-$mess = $_POST['con_message'];
-
-
-
-
-$to = 'themewar@gmail.com';
-$subject = 'Agency User Query';
-
-$message = '<strong>Name : </strong>'.$name.'<br/><br/>';
-$message .= $mess.'<br/>';
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= 'From: <'.$email.'>' . "\r\n";
-
-mail($to,$subject,$message,$headers);
-echo 1;
+<html><head><meta http-equiv=Content-Type content='text/html; charset=UTF-8'></head>
+No data
